5a9aca07a5939501c7b964f737724811acda5fb8
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-03-20 17:10:24 +0900

    feat: Spring Boot 프로젝트 초기 설정
    Spring Initializr를 활용하여 Spring Boot 프로젝트를 생성하였다.
    
    이때, 아래 의존성을 추가하였다.
    - Spring Web
    - Lombok
    - MySQL Driver
    - Spring Data JPA
    이 중 `Spring Data JPA`는 현재 데이터베이스 연결 작업이 되어 있지 않아 오류가 발생해 `gradle.build`에서 주석 처리 하였다.

---
+++ src/main/java/com/sieunp06/customife/CustomifeApplication.java
@@ -0,0 +1,13 @@
+package com.sieunp06.customife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sieunp06/customife/CustomifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sieunp06.customife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
