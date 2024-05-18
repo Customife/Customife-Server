28ea30b264e3ccdaa03270788a948189cf5747b0
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-05-19 02:52:14 +0900

    feat: 이미 존재하는 회원임을 검사하는 기능 구현
    userEmail을 통해 이미 존재하는 회원인지를 검사하고 만약 존재한다면 IllegalStateException을 발생시킨다.

--- src/main/java/com/sieunp06/customife/service/UserService.java
+++ src/main/java/com/sieunp06/customife/service/UserService.java
@@ -19,6 +19,8 @@ public class UserService {
         String encodedPassword = passwordEncoder.encode(userSignupDto.getUserPassword());
         String userNickName = userSignupDto.getUserNickName();
 
+        validateDuplicateUser(userEmail);
+
         User savedUser = userRepository.save(User.builder()
                 .userEmail(userEmail)
                 .userPassword(encodedPassword)
@@ -26,5 +28,12 @@ public class UserService {
                 .build());
 
         return UserResponseDto.from(savedUser);
+    }
+
+    private void validateDuplicateUser(String userEmail) {
+        userRepository.findByUserEmail(userEmail)
+                .ifPresent(error -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 }
