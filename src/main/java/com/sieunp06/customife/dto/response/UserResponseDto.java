f1e5369f040d476ac12f82037d22a78778242ec4
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-05-18 22:24:44 +0900

    refactor: UserResponseDto에서 Password 삭제
    해당 Dto에서 Password를 전달할 이유가 없을 것으로 판단되어 삭제함.

--- src/main/java/com/sieunp06/customife/dto/response/UserResponseDto.java
+++ src/main/java/com/sieunp06/customife/dto/response/UserResponseDto.java
@@ -7,20 +7,17 @@ import lombok.Getter;
 @Getter
 public class UserResponseDto {
     private String userEmail;
-    private String userPassword;
     private String userNickName;
 
     @Builder
-    private UserResponseDto(String userEmail, String userPassword, String userNickName) {
+    private UserResponseDto(String userEmail, String userNickName) {
         this.userEmail = userEmail;
-        this.userPassword = userPassword;
         this.userNickName = userNickName;
     }
 
     public static UserResponseDto from(User user) {
         return UserResponseDto.builder()
                 .userEmail(user.getUserEmail())
-                .userPassword(user.getUserPassword())
                 .userNickName(user.getUserNickName())
                 .build();
     }
